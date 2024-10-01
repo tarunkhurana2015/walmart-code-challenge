430ca5585b254a580073cddd2b42a1fd9aead517
Tarun Khurana <tarunkhurana1982@gmail.com>
2024-10-01 13:02:24 -0500

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCHEME='CountryApp'
-DESTINATION='platform=iOS Simulator,OS=17.2,name=iPhone 15'
+DESTINATION='platform=iOS Simulator,OS=iOS 17.2,name=iPhone 15'
 PROJECT='CountryApp/CountryApp.xcodeproj'
 
 xcodebuild test -project $PROJECT -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
